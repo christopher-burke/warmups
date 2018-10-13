891804a7117f66da1154fc5f3e461556ac50fe4d
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-13 10:39:49 -0400

    Starting Summation of primes.

---
+++ python/warmups/projecteuler/summation_of_primes.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""Summation of primes.
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+
+source: https://projecteuler.net/problem=10
+"""
+
+
+def main(n: int):
+    """Return the sum of primes below n."""
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main(n=200000)
