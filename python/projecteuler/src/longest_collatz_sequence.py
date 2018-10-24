44e5e9bbd24e71f4167ef406b10b70db04c3505b
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-24 16:48:43 -0400

    Added code that solved problem. Needs refactoring.

--- python/projecteuler/src/longest_collatz_sequence.py
+++ python/projecteuler/src/longest_collatz_sequence.py
@@ -9,7 +9,7 @@ for the set of positive integers:
 n → n/2 (n is even)
 n → 3n + 1 (n is odd)
 
-Using the rule above and starting with 13, 
+Using the rule above and starting with 13,
 we generate the following sequence:
 
 13 → 40 → 20 → 10 → 5 → 16 → 8 → 4 → 2 → 1
@@ -28,14 +28,70 @@ source: https://projecteuler.net/problem=14
 """
 
 
-def longest_collatz_sequence():
-    pass
+from pprint import pprint
+from collections import namedtuple
+from itertools import accumulate
+
+
+LongestCollatzSequence = namedtuple(
+    'LongestCollatzSequence',
+    'sequence length')
+
+
+# def longest_collatz_sequence(limit: int = 1000000):
+#
+#    for i in range(2, limit+1):
+#        CACHE[str(i)] = None
+#        sequence = collatz_sequence(i)
+#        CACHE[str(i)] = sequence
+#
+#    return max(CACHE.keys(), key=lambda k: len(CACHE[str(k)]))
+
+
+CACHE = {'1': [1]}
+CACHE_LENGTH = {'1': 1}
+
+
+def longest_collatz_sequence(limit: int = 1000000):
+
+    for i in range(2, limit+1):
+        collatz_sequence_length(i)
+
+    return max(CACHE_LENGTH.keys(), key=lambda k: CACHE_LENGTH[str(k)])
+
+
+def collatz_sequence(n) -> int:
+    """Get the sequence."""
+
+    if CACHE.get(str(n)):
+        return CACHE.get(str(n))
+
+    next_ = int(n // 2) if n % 2 == 0 else int(3 * n + 1)
+
+    sequence = [n] + collatz_sequence(next_)
+    return sequence
+
+
+def collatz_sequence_length(n) -> int:
+    """Get the sequence length."""
+
+    if CACHE_LENGTH.get(str(n)):
+        return CACHE_LENGTH.get(str(n))
+
+    next_ = int(n // 2) if n % 2 == 0 else int(3 * n + 1)
+
+    CACHE_LENGTH[str(n)] = 1 + collatz_sequence_length(next_)
+
+    return CACHE_LENGTH[str(n)]
 
 
 def main():
     """Run Longest Collatz sequence."""
-    longest_collatz_sequence()
+    return longest_collatz_sequence(1000000)
 
 
 if __name__ == "__main__":
-    print(main())
+
+    lcs = main()
+
+    print(lcs, collatz_sequence(int(lcs)))
