9ff186f2bff68024a3c04323fe33d2adc53ed3bf
Christopher Burke <christopherjamesburke@gmail.com>
2018-10-24 23:50:46 -0400

    Improved code. Runs faster.

--- python/projecteuler/src/longest_collatz_sequence.py
+++ python/projecteuler/src/longest_collatz_sequence.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-"""Longest Collatz sequence.
 
+"""Longest Collatz sequence.
 
 The following iterative sequence is defined
 for the set of positive integers:
@@ -28,70 +28,57 @@ source: https://projecteuler.net/problem=14
 """
 
 
-from pprint import pprint
-from collections import namedtuple
-from itertools import accumulate
-
-
-LongestCollatzSequence = namedtuple(
-    'LongestCollatzSequence',
-    'sequence length')
-
-
-# def longest_collatz_sequence(limit: int = 1000000):
-#
-#    for i in range(2, limit+1):
-#        CACHE[str(i)] = None
-#        sequence = collatz_sequence(i)
-#        CACHE[str(i)] = sequence
-#
-#    return max(CACHE.keys(), key=lambda k: len(CACHE[str(k)]))
-
-
-CACHE = {'1': [1]}
-CACHE_LENGTH = {'1': 1}
-
-
-def longest_collatz_sequence(limit: int = 1000000):
-
-    for i in range(2, limit+1):
-        collatz_sequence_length(i)
-
-    return max(CACHE_LENGTH.keys(), key=lambda k: CACHE_LENGTH[str(k)])
+CACHE = {1: [1]}
+CACHE_LENGTH = {1: 1}
 
 
 def collatz_sequence(n) -> int:
-    """Get the sequence."""
+    """Get the Collatz Sequence list.
 
-    if CACHE.get(str(n)):
-        return CACHE.get(str(n))
-
-    next_ = int(n // 2) if n % 2 == 0 else int(3 * n + 1)
-
-    sequence = [n] + collatz_sequence(next_)
-    return sequence
-
-
-def collatz_sequence_length(n) -> int:
-    """Get the sequence length."""
-
-    if CACHE_LENGTH.get(str(n)):
-        return CACHE_LENGTH.get(str(n))
+    Add each found Collatz Sequence to CACHE.
+    :return:
+    """
+    if n in CACHE:
+        return CACHE[n]
 
     next_ = int(n // 2) if n % 2 == 0 else int(3 * n + 1)
+    CACHE[n] = [n] + collatz_sequence(next_)
 
-    CACHE_LENGTH[str(n)] = 1 + collatz_sequence_length(next_)
-
-    return CACHE_LENGTH[str(n)]
+    return CACHE[n]
 
 
-def main():
-    """Run Longest Collatz sequence."""
+def longest_collatz_sequence(limit: int) -> int:
+    """Find the longest Collatz Sequence length.
+
+    :return: number that generates the longest collazt sequence.
+    """
+    for i in range(2, limit+1):
+        collatz_sequence_length(i)
+    longest = max(CACHE_LENGTH.keys(), key=lambda k: CACHE_LENGTH[k])
+
+    return longest
+
+
+def collatz_sequence_length(n):
+    """Get the Collatz Sequence of n.
+
+    :return: List of Collatz Sequence.
+    """
+    if n not in CACHE_LENGTH:
+        next_ = int(n // 2) if n % 2 == 0 else int(3 * n + 1)
+        CACHE_LENGTH[n] = 1 + collatz_sequence_length(next_)
+    return CACHE_LENGTH[n]
+
+
+def main() -> int:
+    """Find the Longest Collatz sequence under 1,000,000.
+
+    :return: Longest Collatz sequence under 1,000,000
+    """
     return longest_collatz_sequence(1000000)
 
 
 if __name__ == "__main__":
-
     lcs = main()
-
-    print(lcs, collatz_sequence(int(lcs)))
+    print(lcs, CACHE_LENGTH[lcs])
+    print(" → ".join(map(str, collatz_sequence(lcs))))
