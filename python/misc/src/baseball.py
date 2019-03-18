c53a0ebc23195a54138d87d778cd6ecee5c8d173
Christopher Burke <christopherjamesburke@gmail.com>
2019-03-18 12:07:03 -0400

    Updated doc strings.

--- python/misc/src/baseball.py
+++ python/misc/src/baseball.py
@@ -9,7 +9,14 @@ from fractions import Fraction
 
 
 def innings(innings: str):
-    """Convert the partial outs to a fraction."""
+    """Convert the partial innings pitched (outs) to a fraction.
+
+    Baseball represents the thirds of innings in the following:
+        * 0.1 = 1 of the 3 outs made, meaning 1/3.
+        * 0.2 = 2 of the 3 outs made, meaning 2/3.
+
+    These fractions need to be converted properly in order to be processed.
+    """
     try:
         innings, fraction = innings.split('.')
         fraction = Fraction(int(fraction), 3)
@@ -23,6 +30,8 @@ def era(innings_pitched: float, er: int, total_innings: int=9) -> float:
 
     ERA = ('earned runs' / 'innings pitched') * 'total innings'
 
+    er = Earned Runs
+
     """
     ERA = (er * total_innings) / innings(str(innings_pitched))
     return round(float(ERA), 2)
@@ -32,6 +41,9 @@ def whip(innings_pitched: float, bb: int, h: int) -> float:
     """Calculate a baseball pitcher's WHIP.
 
     WHIP = (BB + H) / IP
+
+    bb = Walks / Base on balls
+    h = hits
 
     """
     WHIP = (bb + h) / innings(str(innings_pitched))
