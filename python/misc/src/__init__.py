4dea49151d0a114714643f968e262fb1b1c29145
Christopher Burke <christopherjamesburke@gmail.com>
2019-04-26 23:25:14 -0400

    Turned misc into python package.

---
+++ python/misc/src/__init__.py
@@ -0,0 +1,49 @@
+from .accumulating_product import accumulating_product_reduce, accumulating_product_acc, accumulating_product
+from .anagram import letter_count, anagram
+from .atm_pin_code_validation import is_valid_pin
+from .baseball import innings, era, whip
+from .char_to_ascii import ctoa
+from .chocolate_distribution_problem import chunks
+from .convert_to_decimal_notation import convert_to_decimal
+from .count_args import num_args
+from .days_in_month import days_in_month
+from .decode_message import decode, total_number_messages_decoded
+from .doctors_clinic import last_patient_wait_time
+from .equilibrium_point import equilibrium
+from .find_element_that_appears_only_once import find_unique_value
+from .find_sum import find_sum_naive, find_sum
+from .find_the_odd_integer import find_odd
+from .first_repeated_char import first_repeated_char_v1, first_repeated_char_v2
+from .format_number import grouper, format_number
+from .initialize import __initialize, initialize
+from .k_th_smallest_element import partition, find_element_postion
+from .kadanes_algorithm import max_subarray_sum
+from .largest_number_formed import largest_number
+from .leaders_in_an_array import find_leaders
+from .max_of_all_subarrays_of_size_k import maximum_of_all_subarrays
+from .max_of_three import find_max
+from .maximum_sum_increasing_subsequence import max_sum_subsequence
+from .merge_sort_example import merge, merge_sort
+from .middle_element import middle
+from .min_max_length_average import minMaxLengthAverage
+from .minimum_platforms import minimum_platforms
+from .missing_num_in_array import find_missing_number
+from .missing_third_angle import calc_missing_angle, angle_classifier, missing_angle
+from .multiply_by_length import multiply_by_length
+from .number_to_month import month_name
+from .parenthesis_checker import parenthesis_checker
+from .password_generator import random_int, generate_password
+from .permutations import itertools_permutations, permutations_
+from .profitable_gamble import profitable_gamble
+from .remove_duplicates import remove_duplicates
+from .repeat_letters_n_times import repeat
+from .reverse_words_in_a_given_string import reverse_words_in_string
+from .rotate_list import rotate
+from .slices_of_pie import equal_slices
+from .sort_array_012 import sort_array_012
+from .stock_buy_sell import maximum_profit
+from .subarray_with_given_sum import subarray_with_given_sum
+from .symmetric_difference import symmetric_difference_list, symmetric_difference
+from .valid_zip_code import is_valid
+from .vowel_consonant_replacer import replace_vowels, replace_consonants
+from .zig_zag import zig_zag, zig_zag_both
