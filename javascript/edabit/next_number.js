99c6d32fcb0709d7fc07bb4b93e154fba81e35ed
Christopher Burke <christopherjamesburke@gmail.com>
2022-03-02 21:31:13 -0500

    Javascript Return the Next Number from the Integer Passed.

---
+++ javascript/edabit/next_number.js
@@ -0,0 +1,11 @@
+/*
+Return the Next Number from the Integer Passed.
+
+Create a function that takes a number as an argument, increments the number by +1 and returns the result.
+
+https://edabit.com/challenge/NAQhEoxbofPidLxm9
+*/
+
+function addition(num) {
+    return num + 1;
+}
